7307ee015d66971e6c369b86e45ae23299b72b39
Danil <86056760+comelovedo@users.noreply.github.com>
2022-07-25 14:44:27 +0200

    Update Registration.py

--- page/Registration.py
+++ page/Registration.py
@@ -4,12 +4,12 @@ from page.Locators.registration_locators import Login_Locators
 
 class Login_Page(BasePage):
     def login_valid_account(self):
-        self.type(Login_Locators.NAME_INPUT, 'Danil_Borisov@rossum.ai')
-        self.type(Login_Locators.PASS_INPUT, 'Creative42435154')
+        self.type(Login_Locators.NAME_INPUT, 'YourEmail')
+        self.type(Login_Locators.PASS_INPUT, 'YourPassword')
         self.click(Login_Locators.SUBMIT_ACCOUNT)
 
     def login_invalid_account(self):
-        self.type(Login_Locators.NAME_INPUT, 'Borisov_Danil@rossum.ai')
-        self.type(Login_Locators.PASS_INPUT, 'Creative')
+        self.type(Login_Locators.NAME_INPUT, 'YourEmail')
+        self.type(Login_Locators.PASS_INPUT, 'YourPassword')
         self.click(Login_Locators.SUBMIT_ACCOUNT)
 
